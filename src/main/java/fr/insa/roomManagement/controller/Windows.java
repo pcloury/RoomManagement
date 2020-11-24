ed91a95d9eef6cba9ff6ed70b50bcca1923fb892
pcloury <pierre.cloury@gmail.com>
2020-11-24 15:00:50 +0100

    ajout des services luminosity et window

---
+++ src/main/java/fr/insa/roomManagement/controller/Windows.java
@@ -0,0 +1,16 @@
+package fr.insa.roomManagement.controller;
+
+public class Windows {
+	boolean windowOpen;
+	boolean heatingOn;
+
+	public Windows() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean checkWindow() {
+		return windowOpen;
+		
+	}
+
+}
