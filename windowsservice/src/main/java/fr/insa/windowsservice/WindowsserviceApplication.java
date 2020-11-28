ca6384fb97cd64125e0887e119252903cee52175
pcloury <pierre.cloury@gmail.com>
2020-11-28 18:17:20 +0100

    ajout windowsservice

---
+++ windowsservice/src/main/java/fr/insa/windowsservice/WindowsserviceApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.windowsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowsserviceApplication.class, args);
+	}
+
+}
